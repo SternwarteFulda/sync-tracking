9bf7182161502762c6c160e178665e71d648e82d
Marcus Holland-Moritz <gitlab@mhxnet.de>
2020-01-16 15:04:52 +0100

    Add experimental one second startup period

--- tracking.c
+++ tracking.c
@@ -33,6 +33,8 @@ static struct generator_state s_gen;
 static uint8_t s_output;
 static uint16_t s_power;
 static uint16_t s_new_index_timer;
+static uint16_t s_startup_timer;
+static uint8_t s_startup_finished;
 
 ISR(TIMER_OVERFLOW_VECTOR) {
   PWM_OUTPUT_REG = s_output ? generator_generate(&s_gen) : 127;
@@ -47,6 +49,12 @@ ISR(TIMER_OVERFLOW_VECTOR) {
 
   if (s_new_index_timer > 0) {
     s_new_index_timer--;
+  }
+
+  if (s_startup_timer > 0) {
+    if (--s_startup_timer == 0) {
+      s_startup_finished = 1;
+    }
   }
 }
 
@@ -76,7 +84,10 @@ static void idle_loop(void) {
     uint8_t pin = (~PINA) & 0xF0;
 
     if (pin != last_pin
-        || (new_index != active_index && s_new_index_timer == 0)) {
+        || (new_index != active_index && s_new_index_timer == 0)
+        || s_startup_finished) {
+      s_startup_finished = 0;
+
       uint8_t index = pin >> 6;
 
       if (index != new_index) {
@@ -93,6 +104,11 @@ static void idle_loop(void) {
       }
 
       if (new_index != active_index && s_new_index_timer == 0) {
+        if (active_index == 0) {
+          STATIC_ASSERT(PWM_FREQ <= 0xFFFF);
+          s_startup_timer = PWM_FREQ;
+        }
+
         active_index = new_index;
       }
 
@@ -122,7 +138,7 @@ static void idle_loop(void) {
         cli();
         s_output = 1;
         generator_set_phase_increment(&s_gen, phase_inc[index]);
-        generator_set_sine(&s_gen, sine_data[index]);
+        generator_set_sine(&s_gen, s_startup_timer > 0 ? sine_fast : sine_data[index]);
         sei();
       } else {
         // Output is disabled
